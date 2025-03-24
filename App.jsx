5dc0f2269a4c4b063aa8cfee1f56b11b209eec0b
Mac <mac@Macs-Mac-mini.local>
2025-03-24 17:21:00 +0500

    Removed unnecessary app warning logs

--- App.jsx
+++ App.jsx
@@ -1,8 +1,11 @@
 import 'react-native-url-polyfill/auto.js';
+import {LogBox} from 'react-native';
 import Parent from "./src/screens/ParentScreen/Parent.jsx";
 
 import {Provider} from 'react-redux';
 import {store} from './src/redux/store.js';
+
+LogBox.ignoreAllLogs(true);
 
 const App = () => {
   return(
